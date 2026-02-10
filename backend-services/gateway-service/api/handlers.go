cecb108c96494f7eeb0ecd06e6af1d509ef0ddc2
abeb021 <albertars021@gmail.com>
2026-02-10 13:14:24 +0300

    JWT middleware partially done

--- backend-services/gateway-service/api/handlers.go
+++ backend-services/gateway-service/api/handlers.go
@@ -10,8 +10,8 @@ func RegisterRoutes (r *http.ServeMux, srv *service.Service) {
 	r.Handle("POST /api/auth/register", srv.AuthProxy())
 	r.Handle("POST /api/auth/login", srv.AuthProxy())
 
-	r.Handle("/api/auth", middleware.JWTMiddleware(srv.AuthProxy()))
-	r.Handle("/api/message", middleware.JWTMiddleware(srv.AuthProxy()))
+	r.Handle("/api/auth", middleware.JWTMiddleware(srv.JWTSecret, srv.AuthURL, srv.AuthProxy()))
+	r.Handle("/api/message", middleware.JWTMiddleware(srv.JWTSecret, srv.AuthURL, srv.AuthProxy()))
 }
 
 
--- backend-services/gateway-service/api/middleware/JWTmiddleware.go
+++ backend-services/gateway-service/api/middleware/JWTmiddleware.go
@@ -1,14 +1,76 @@
 package middleware
 
-import "net/http"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 
-func JWTMiddleware(next http.Handler) http.Handler{
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-		token := r.Header.Get("Authorization")
-		if token == ""{
-			http.Error(w, "Unathorized", http.StatusUnauthorized)
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func JWTMiddleware(secret, authServiceURL string, next http.Handler) http.HandlerFunc{
+	return func (w http.ResponseWriter, r *http.Request) {
+		//skip options
+		if r.Method == http.MethodOptions{
+			next.ServeHTTP(w, r)
+		}
+
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == ""{
+			http.Error(w, "Missing authorization header", http.StatusUnauthorized)
 			return
 		}
+		
+		separated := strings.Split(authHeader, " ")
+		if len(separated) != 2 || separated[0] != "Bearer"{
+			http.Error(w, "Malformed authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.ParseWithClaims(separated[1], &jwt.RegisteredClaims{}, func (token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+		if err != nil || !token.Valid{
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			log.Println("2")
+			return
+		}
+
+		userID, err := token.Claims.GetSubject()
+		if err != nil || userID == ""{
+			http.Error(w, "Empty Token", http.StatusUnauthorized)
+			log.Println("3")
+			return
+		}
+
+		authURL := fmt.Sprintf("%s/auth/user/%s", authServiceURL, userID)
+		req, err := http.NewRequest(http.MethodGet, authURL, nil)
+		if err != nil{
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil{
+			http.Error(w, "Failed to identify user", http.StatusUnauthorized)
+			return
+		}
+		resp.Body.Close()
+
+		if resp.StatusCode == http.StatusNotFound {
+			http.Error(w, "Could not find the user", http.StatusUnauthorized)
+			return
+		}
+		if resp.StatusCode != http.StatusAccepted {
+			http.Error(w, "Failed to identify user", http.StatusUnauthorized)
+			return
+		}
+		r.Header.Set("X-User-ID", userID)
+		ctx := context.WithValue(r.Context(), "userID", userID)
+		r = r.WithContext(ctx)
 		next.ServeHTTP(w, r)
-	})
+	}
 }
